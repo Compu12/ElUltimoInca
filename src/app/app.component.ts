3d068b4ccd1f58094ba6d2018ef1f560fba2d381
Xavier Cañizares <xavier.canizares.95@gmail.com>
2024-02-22 15:08:28 -0500

    feat: create project
    create project and estucture pages with section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HistoryComponent } from './components/history/history.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,HeroSectionComponent,ProductsComponent,ContactComponent,HistoryComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ElUltimoInca';
---
+++ src/app/components/testimonies/testimonies.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-testimonies',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './testimonies.component.html',
+  styleUrls: ['./testimonies.component.css']
+})
+export class TestimoniesComponent {
+
+}
